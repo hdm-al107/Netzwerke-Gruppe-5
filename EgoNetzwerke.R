ecca47bdbb3d9e180ec62b3c6c7f186be407d817
hdm-al107 <37869826+hdm-al107@users.noreply.github.com>
2018-11-14 11:40:08 +0100

    Add files via upload

---
+++ EgoNetzwerke.R
@@ -0,0 +1,49 @@
+#Skript zum Einfügen in R
+library("igraph")
+el=read.csv("https://raw.githubusercontent.com/hdm-al107/Netzwerke-Gruppe-5/master/Hochschulr%C3%A4te_Edgelist_BEREINIGT.csv")
+el
+#Nodelist einfügen
+nodes=read.csv("https://raw.githubusercontent.com/hdm-al107/Netzwerke-Gruppe-5/master/Hochschulr%C3%A4te_Nodelist_BEREINIGT.csv")
+nodes
+# Umwnadlung der Listen in Matrixe
+head(el)
+head(nodes)
+# addiert edges auf, wenn sie auf der gleichen Beziehung sind
+hties <-as.matrix(el)
+hochschulen <- graph_from_data_frame(d=hties, vertices=nodes,
+                                     directed=T)
+plot(hochschulen)
+plot(hochschulen, edge.arrow.size=0.4, vertex.label=NA)
+plot(hochschulen, edge.arrow.size=0.4,
+     vertex.label=V(hochschulen)=id)
+
+plot(hochschulen, edge.arrow.size=0.4, vertex.label=V(hochschulen)=id)
+
+V(hochschulen)$color = "yellow"
+male <- V(hochschulen)[sex == "2"]
+V(hochschulen)[male]$color = "deepskyblue"
+
+female <- V(hochschulen)[sex == "1"]
+V(hochschulen)[female]$color = "hotpink"
+
+org <- V(hochschulen)[type == "1"]
+V(hochschulen)[org]$color = "lawngreen"
+
+hs <- V(hochschulen)[type == "2"]
+V(hochschulen)[hs]$color = "khaki1"
+
+plot(hochschulen, vertex.label=NA)
+
+dyad_census(hochschulen)
+triad_census(hochschulen)
+
+V(hochschulen)$shape = "circle"
+organizations <- V(hochschulen)[type == "1"]
+V(hochschulen)[organizations]$shape = "square"
+
+E(hochschulen)$arrow.size <- .2
+E(hochschulen)$curved=.2
+
+ego(hochschulen)
+mrst <- make_ego_graph(hochschulen, order=2, nodes= V(hochschulen)$name == "BEBN", mode="all")
+plot(mrst[[1]])
